10814ce7356a0b288dbefa53a6e1e7cb0fd36bc1
Luiz Pires - Ranzinza <luiz.pires@ranzinza.dev>
2023-04-15 15:50:23 -0300

    chore: add ConfigService config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './config/env.validation';
+import config from './config/config';
+
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ validate, isGlobal: true, load: [config] })],
   controllers: [],
   providers: [],
 })
